201429f1f92f7e51ce4d7de15637bbb0ce85a8f3
Uranite <62639703+Uranite@users.noreply.github.com>
2024-10-03 21:59:21 +0700

    svt-av1-psy v2.2.1-B and aom-psy101 df18b42

--- scripts.d/50-aom.sh
+++ scripts.d/50-aom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_REPO="https://aomedia.googlesource.com/aom"
-SCRIPT_COMMIT="a42ea198de1e1438f5ee462bd599d082c924bc35"
+SCRIPT_REPO="https://gitlab.com/damian101/aom-psy101.git"
+SCRIPT_COMMIT="df18b42d03624f5c9fac2ef9211a66413b8dd88e"
 
 ffbuild_enabled() {
     [[ $TARGET == winarm64 ]] && return -1
