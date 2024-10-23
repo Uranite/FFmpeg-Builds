6b0725dc62e19d8f7c620837734bebf336c37629
Uranite <62639703+Uranite@users.noreply.github.com>
2024-10-23 15:27:19 +0700

    update deps (partial)
    svt-av1-psy bb886b2 and aom-psy101 a0178dfc

--- scripts.d/25-libxml2.sh
+++ scripts.d/25-libxml2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_REPO="https://github.com/GNOME/libxml2.git"
-SCRIPT_COMMIT="d67833a3c5db7999820a73e356327d47ec76bea9"
+SCRIPT_COMMIT="944e5fe8df2efac3b483f896b3cc350c10fa1c1a"
 
 ffbuild_enabled() {
     return 0
